f263b665c0c353681aaf6d340657dfc9a35351a0
Rodolfo Carvalho <rhcarvalho@gmail.com>
2015-07-08 15:11:07 +0200

    WIP move common code to separate repo

--- 2.0/test/run_test.go
+++ 2.0/test/run_test.go
@@ -1,41 +1,34 @@
 package test
 
 import (
-	"os/exec"
 	"path"
 	"path/filepath"
 	"runtime"
 	"testing"
 
-	"github.com/openshift/sti-ruby/2.0/test/imagetest"
+	"github.com/openshift/imagetest"
 )
 
-var TestDir string
+const BuilderImage = "openshift/ruby-20-centos7-candidate"
+
+var RepoRoot string
 
 func init() {
 	_, filename, _, _ := runtime.Caller(1)
-	TestDir = path.Dir(filename)
+	RepoRoot = path.Dir(path.Dir(path.Dir(filename)))
 }
 
 func TestRubyHelloWorld(t *testing.T) {
 	t.Parallel()
-	if !imagetest.ReuseImages {
-		cmd := exec.Command("git", "clone", "git://github.com/openshift/ruby-hello-world.git")
-		cmd.Dir = TestDir
-		b, err := cmd.CombinedOutput()
-		if err != nil {
-			t.Fatalf("\n%s\n%s", b, err)
-		}
-	}
-	imagetest.TestImageFromSource(t, filepath.Join(TestDir, "ruby-hello-world"))
+	imagetest.TestImageFromSource(t, BuilderImage, "git://github.com/openshift/ruby-hello-world.git", "", "sti-ruby-test/ruby-hello-world")
 }
 
 func TestPuma(t *testing.T) {
 	t.Parallel()
-	imagetest.TestImageFromSource(t, filepath.Join(TestDir, "puma-test-app"))
+	imagetest.TestImageFromSource(t, BuilderImage, RepoRoot, filepath.Join("2.0", "test", "puma-test-app"), "sti-ruby-test/puma-test-app")
 }
 
 func TestRack(t *testing.T) {
 	t.Parallel()
-	imagetest.TestImageFromSource(t, filepath.Join(TestDir, "rack-test-app"))
+	imagetest.TestImageFromSource(t, BuilderImage, RepoRoot, filepath.Join("2.0", "test", "rack-test-app"), "sti-ruby-test/rack-test-app")
 }
