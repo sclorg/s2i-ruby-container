fd6794ef3f8d36a0880de72211de595c2130dbac
Petr "Stone" Hracek <phracek@redhat.com>
2022-09-29 13:23:35 +0200

    Fix #422: Allow testing on RHEL8
    Closes #422
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test-lib-ruby.sh
+++ test/test-lib-ruby.sh
@@ -21,7 +21,7 @@ function test_ruby_integration() {
 # Check the imagestream
 function test_ruby_imagestream() {
   case ${OS} in
-    rhel7|centos7) ;;
+    rhel7|centos7|rhel8) ;;
     *) echo "Imagestream testing not supported for $OS environment." ; return 0 ;;
   esac
 
