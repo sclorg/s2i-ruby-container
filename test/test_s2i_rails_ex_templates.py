71aac15ab084a4688d8c7662222703eeda041c58
Petr Hracek <phracek@redhat.com>
2024-07-04 11:21:47 +0200

    Add container-ci-suite for testing ruby-container in Openshift 4 (#551)
    * Add PyTest suite for testing ruby container in OpenShift 4
    
    * Add symlinks to each versions
    
    * Get rid of testing OpenShift 3
    
    * Get rid of conu. It is not used at all. Remove test_s2i.py from version 3.3 as well.
    
    ---------
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ test/test_s2i_rails_ex_templates.py
@@ -0,0 +1,78 @@
+import os
+import sys
+
+import pytest
+
+from container_ci_suite.openshift import OpenShiftAPI
+from container_ci_suite.utils import check_variables
+
+if not check_variables():
+    print("At least one variable from IMAGE_NAME, OS, SINGLE_VERSION is missing.")
+    sys.exit(1)
+
+
+VERSION = os.getenv("SINGLE_VERSION")
+IMAGE_NAME = os.getenv("IMAGE_NAME")
+OS = os.getenv("TARGET")
+
+DEPLOYED_PGSQL_IMAGE = "quay.io/sclorg/postgresql-12-c8s"
+
+TAGS = {
+    "rhel8": "-el8",
+    "rhel9": "-el9"
+}
+TAG = TAGS.get(OS, None)
+IMAGE_SHORT = f"postgresql:12{TAG}"
+IMAGE_TAG = f"12{TAG}"
+
+
+class TestS2IRailsExTemplate:
+
+    def setup_method(self):
+        self.oc_api = OpenShiftAPI(pod_name_prefix="ruby-testing", version=VERSION)
+        assert self.oc_api.upload_image(DEPLOYED_PGSQL_IMAGE, IMAGE_SHORT)
+
+    def teardown_method(self):
+        self.oc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "template",
+        [
+            "rails.json",
+            "rails-postgresql-persistent.json"
+        ]
+    )
+    def test_rails_template_inside_cluster(self, template):
+        service_name = "ruby-testing"
+        rails_ex_branch = "master"
+        if VERSION == "3.3":
+            rails_ex_branch = VERSION
+        template_url = self.oc_api.get_raw_url_for_json(
+            container="rails-ex", dir="openshift/templates", filename=template, branch=rails_ex_branch
+        )
+        openshift_args = [
+            f"SOURCE_REPOSITORY_URL=https://github.com/sclorg/rails-ex.git",
+            f"SOURCE_REPOSITORY_REF={rails_ex_branch}",
+            f"RUBY_VERSION={VERSION}",
+            f"NAME={service_name}"
+        ]
+        if template != "rails.json":
+            openshift_args = [
+                f"SOURCE_REPOSITORY_URL=https://github.com/sclorg/rails-ex.git",
+                f"SOURCE_REPOSITORY_REF={rails_ex_branch}",
+                f"POSTGRESQL_VERSION={IMAGE_TAG}",
+                f"RUBY_VERSION={VERSION}",
+                f"NAME={service_name}",
+                f"DATABASE_USER=testu",
+                f"DATABASE_PASSWORD=testp"
+            ]
+        assert self.oc_api.deploy_template_with_image(
+            image_name=IMAGE_NAME,
+            template=template_url,
+            name_in_template="ruby",
+            openshift_args=openshift_args
+        )
+        assert self.oc_api.template_deployed(name_in_template=service_name)
+        assert self.oc_api.check_response_inside_cluster(
+            name_in_template=service_name, expected_output="Welcome to your Rails application"
+        )
