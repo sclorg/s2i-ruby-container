fa7b893831498b395ebcf26d1211f439cb1e0000
Petr "Stone" Hracek <phracek@redhat.com>
2025-02-12 11:51:48 +0100

    Specify which tests will be running on shared_cluster.
    Postgresql tests are failing for issue with permissions
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_helm_ruby_rails_application.py
+++ test/test_helm_ruby_rails_application.py
@@ -28,12 +28,12 @@ TAGS = {
 TAG = TAGS.get(OS, None)
 
 
-class TestHelmCakePHPTemplate:
+class TestHelmRubyTemplate:
 
     def setup_method(self):
         package_name = "redhat-ruby-rails-application"
         path = test_dir
-        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, remote=True)
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir, shared_cluster=True)
         self.hc_api.clone_helm_chart_repo(
             repo_url="https://github.com/sclorg/helm-charts", repo_name="helm-charts",
             subdir="charts/redhat"
