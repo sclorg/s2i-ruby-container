5c556b4e15657e3d0c0dcf8826490ef085fab192
Petr "Stone" Hracek <phracek@redhat.com>
2026-04-28 13:08:38 +0200

    Increase timeout
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_ocp_s2i_templates.py
+++ test/test_ocp_s2i_templates.py
@@ -70,7 +70,7 @@ class TestS2IRailsTemplates:
             name_in_template="ruby",
             openshift_args=openshift_args,
         )
-        assert self.oc_api.is_template_deployed(name_in_template=service_name)
+        assert self.oc_api.is_template_deployed(name_in_template=service_name, timeout=480)
         assert self.oc_api.check_response_inside_cluster(
             name_in_template=service_name,
             expected_output="Welcome to your Rails application",
